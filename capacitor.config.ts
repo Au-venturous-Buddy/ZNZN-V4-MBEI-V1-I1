865562e8ecb268a09c08d804cffef147e6670b2c
Au-venturous-Buddy <130553802+Au-venturous-Buddy@users.noreply.github.com>
2024-11-04 19:38:12 +0800

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.i0.v0.mbei.znzn',
-  appName: "Zene 'N Zeanne Social Snapshots Template",
+  appId: 'com.i1.v1.mbei.v4.znzn',
+  appName: "Zene 'N Zeanne Social Snapshots: Vaccines",
   webDir: 'public',
   bundledWebRuntime: false
 };
